59b039637b0e396d2ada10868ec7b7e31d5159e8
HiNT89 <hint8901@gmail.com>
2022-06-22 16:37:47 +0700

    a

---
+++ js/js.js
@@ -0,0 +1,6 @@
+const nav = document.querySelector('#nav-mobile');
+const menuMobile=document.querySelector('#menu-mobile-btn');
+
+menuMobile.onclick = function () {
+    nav.style.display = "block";
+}
